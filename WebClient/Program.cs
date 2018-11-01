32ded1d6ed3bdcb0a2baa2251797774765b938d2
Jon Bolkema <jrbolkema@gmail.com>
2018-11-01 13:12:41 -0600

    part 3

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WebClient
@@ -11,26 +12,72 @@ namespace WebClient
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("open of program");
+			string responseString;
+			string portInput;
+			short portNumber;
+
+
 			using (HttpListener listener = new HttpListener())
 			{
-				UriBuilder myUri = new UriBuilder("http", "localhost", 8081tt);
+				// Checking if args is empty
+				if (args.Length == 0)
+				{
+					portNumber = 4200;
+					Console.WriteLine("No port number provided, defaulting to 4200.");
+				}
+				else
+				{
+
+
+					portInput = Convert.ToString(args[0]);
+
+					// Checking if given argument meets 4 digit format.
+					if (Regex.IsMatch(portInput, @"^[1-9]{1}\d{3}$"))
+					{
+						portNumber = Convert.ToInt16(portInput);
+					}
+					else
+					{
+						throw new FormatException("Doesn't match 4-digit port format.");
+					}
+
+				}
+
+				// Building the URI
+				UriBuilder myUri = new UriBuilder("http", "localhost", portNumber);
 				listener.Prefixes.Add(myUri.ToString());
 				listener.Start();
-				Console.WriteLine("Working, before blocks");
-				// Note: The GetContext method blocks while waiting for a request.
+				
+
+				// The GetContext method blocks while waiting for a request.
 				HttpListenerContext context = listener.GetContext();
 				HttpListenerRequest request = context.Request;
-				Console.WriteLine("after blocks");
+				HttpListenerResponse response = context.Response;
 
-
-				string responseString = "Tester";
+				// Making sure values given can be added
+				try
+				{
+					var x = Convert.ToInt32(request.QueryString.GetValues(0)[0]);
+					var y = Convert.ToInt32(request.QueryString.GetValues(1)[0]);
+					responseString = Convert.ToString(x + y);
+				}
+				catch (Exception e)
+				{
+					responseString = "The provided values were invalid";
+					Console.WriteLine(responseString);
+					throw e;
+				}
+				
 
 				// Get a response stream and write the response to it.
 				byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-				var response = context.Response;
+				string result = Encoding.UTF8.GetString(buffer);
 				var output = response.OutputStream;
-				output.Write(buffer,0,buffer.Length);
+				output.Write(buffer, 0, buffer.Length);
+
+				// Cleaning up
+				output.Close();
+				listener.Stop();
 			}
 
 		}
